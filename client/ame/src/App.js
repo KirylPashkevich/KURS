69556f67da0ae7a89eb53c61af04e57602ee2809
KirylPashkevich <pakir2005@gmail.com>
2025-02-18 21:19:42 +0300

    added routes

--- client/ame/src/App.js
+++ client/ame/src/App.js
@@ -1,24 +1,103 @@
-import logo from './logo.svg';
-import './App.css';
+
+
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Registration from './components/Registration';
+import Login from './components/Login';
+import Dashboard from './components/Dashboard';
+import HomePage from './components/HomePage';
+import SupplierDetails from './components/SupplierDetails';
+import SupplierAdmin from './components/SupplierAdmin';
+import SupplierOrders from './components/SupplierOrders';
+import PrivateRoute from './components/PrivateRoute';
+import SupplierRoute from './components/SupplierRoute';
+import EditSupplierInfo from './components/EditSupplierInfo';
+import EquipmentList from './components/EquipmentList';
+import AddEquipment from './components/AddEquipment';
+import EditEquipment from './components/EditEquipment';
+import Cart from './components/Cart';
+import Orders from './components/Orders';
+import Profile from './components/Profile';
+import { Container } from '@mui/material';
+import NavigationBar from './components/NavigationBar';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <NavigationBar />
+      <Container maxWidth="lg" sx={{ paddingY: 4 }}>
+        <Routes>
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/orders" element={<Orders />} />
+          <Route path="/" element={<HomePage />} />
+          {/* Детали поставщика */}
+          <Route path="/supplier/:id" element={<SupplierDetails />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/register" element={<Registration />} />
+          <Route path="/login" element={<Login />} />
+          <Route
+            path="/dashboard"
+            element={
+              <PrivateRoute>
+                <Dashboard />
+              </PrivateRoute>
+            }
+          />
+          {/* Роуты для поставщика */}
+          <Route
+            path="/supplier-admin"
+            element={
+              <SupplierRoute>
+                <SupplierAdmin />
+              </SupplierRoute>
+            }
+          />
+          <Route
+            path="/supplier-admin/edit"
+            element={
+              <SupplierRoute>
+                <EditSupplierInfo />
+              </SupplierRoute>
+            }
+          />
+          <Route
+            path="/supplier-admin/equipments"
+            element={
+              <SupplierRoute>
+                <EquipmentList />
+              </SupplierRoute>
+            }
+          />
+          <Route
+            path="/supplier-admin/equipments/add"
+            element={
+              <SupplierRoute>
+                <AddEquipment />
+              </SupplierRoute>
+            }
+          />
+          <Route
+            path="/supplier-admin/equipments/edit/:id"
+            element={
+              <SupplierRoute>
+                <EditEquipment />
+              </SupplierRoute>
+            }
+          />
+          <Route
+            path="/supplier-admin/orders"
+            element={
+              <SupplierRoute>
+                <SupplierOrders />
+              </SupplierRoute>
+            }
+          />
+        </Routes>
+      </Container>
+      <ToastContainer />
+    </>
   );
 }
 
